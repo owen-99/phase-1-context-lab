aac7bf15c69bf47d8dc72064dfbb69776b649c7f
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-20 04:07:12 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
 /* Your Code Here */
+function createEmployeeRecord(recordArr) {
+    return {
+        firstName: recordArr[0],
+        familyName: recordArr[1],
+        title: recordArr[2],
+        payPerHour: recordArr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+
+function createEmployeeRecords(arrOfRecordArr) {
+    return arrOfRecordArr.map(recordArr => createEmployeeRecord(recordArr));
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+
+    const timeInObject = {
+        type: 'TimeIn',
+        hour: +hour,
+        date: date,
+    }
+
+    this.timeInEvents.push(timeInObject);
+    return this;
+}
+
+
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+
+    const timeOutObject = {
+        type: 'TimeOut',
+        hour: +hour,
+        date: date,
+    }
+
+    this.timeOutEvents.push(timeOutObject);
+    return this;
+}
+
+
+function hoursWorkedOnDate(dateNoHour) {
+    const timeInHour = this.timeInEvents.find(dateStamp => dateStamp.date === dateNoHour).hour;
+    const timeOutHour = this.timeOutEvents.find(dateStamp => dateStamp.date === dateNoHour).hour;
+
+    return ((timeOutHour - timeInHour) / 100);
+}
+
+function wagesEarnedOnDate(dateNoHour){
+    const wagesOnDate = hoursWorkedOnDate.call(this, dateNoHour)
+    return wagesOnDate * this.payPerHour
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,5 +75,13 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(arrOfRecordArr, firstName){
+    return arrOfRecordArr.find(arr => arr.firstName === firstName)
+}
+
+function calculatePayroll(arrOfRecordArr) {
+    return arrOfRecordArr.reduce((ticket, recordArr) => ticket + allWagesFor.call(recordArr), 0)
 }
 
